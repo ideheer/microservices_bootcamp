095443a9995a63653d1f4b15f3684d0568ea9495
Ian de Heer <ian.deheer@gmail.com>
2024-06-27 10:43:55 -0700

    create new book

--- main.js
+++ main.js
@@ -6,8 +6,11 @@ const path = require('path'); // Importe o módulo 'path'
 app.use(express.json()); // for parsing application/json
 
 const authors = [];
+const books = [];
 let authorCounter = 0;
+let bookCounter = 0;
 
+//Create author
 app.post('/authors', (req, res) => {
   const newAuthor = {
     "id": authorCounter,
@@ -20,10 +23,12 @@ app.post('/authors', (req, res) => {
   res.send(req.body);
 });
 
+//Get all authors
 app.get('/authors', (req, res) => {
   res.json(authors);
 });
 
+//Get author by ID
 app.get('/authors/:id', (req, res) => {
   let authorId = req.params.id;
   console.log('get /authors/:id', authors[authorId]);
@@ -33,6 +38,7 @@ app.get('/authors/:id', (req, res) => {
   else(res.status(404).send('Not found'));
 });
 
+//Update author by ID
 app.put('/authors/:id', (req, res) => {
   let authorId = req.params.id;
   if(authors[authorId]){
@@ -41,8 +47,37 @@ app.put('/authors/:id', (req, res) => {
     authors[authIndex].name = req.body.name;
     authors[authIndex].bio = req.body.bio;
     console.log("to: ", authors[authIndex])
+    res.json(authors[authorId])
   }
   else(res.status(404).send('Not found'));
+});
+
+//Delete author by ID
+app.delete('/authors/:id', (req, res) => {
+  let authorId = req.params.id;
+  if(authors[authorId]){
+    authIndex = authors.findIndex(obj => obj.id == authorId);
+    console.log("Author to be deleted: ", authors[authIndex]);
+    delete authors[authIndex];
+    console.log("Author deleted");
+    res.send("Author removed")
+  }
+  else(res.status(404).send('Not found'));
+})
+
+//Create book
+app.post('/books', (req, res) => {
+  const newBook = {
+    "id": bookCounter,
+    "title": req.body.title,
+	  "authorId": req.body.authorId,
+	  "publishedDate": req.body.publishedDate,
+	  "summary": req.body.summary
+  };
+  books.push(newBook);
+  console.log(req.body);
+  bookCounter++;
+  res.send(req.body);
 });
 
 const port = process.env.PORT || 3000;
