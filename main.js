4ad834a410edf938ada35ac7860dcb6e182c7a75
Ian de Heer <ian.deheer@gmail.com>
2024-06-27 18:28:07 -0700

    Finished first endpoints

--- main.js
+++ main.js
@@ -80,6 +80,51 @@ app.post('/books', (req, res) => {
   res.send(req.body);
 });
 
+//Get all books
+app.get('/books', (req, res) => {
+  res.json(books);
+});
+
+//Get book by ID
+app.get('/books/:id', (req, res) => {
+  let bookId = req.params.id;
+  console.log('get /books/:id', books[bookId]);
+  if(books[bookId]){
+    res.json(books[bookId]);
+  }
+  else(res.status(404).send('Not found'));
+});
+
+//Update book by ID
+app.put('/books/:id', (req, res) => {
+  let bookId = req.params.id;
+  if(books[bookId]){
+    bookIndex = books.findIndex(obj => obj.id == bookId);
+    console.log("Book updated from: ", books[bookIndex]);
+    books[bookIndex].title = req.body.title;
+    books[bookIndex].authorId = req.body.authorId;
+    books[bookIndex].publishedDate = req.body.publishedDate;
+    books[bookIndex].summary = req.body.summary;
+    console.log("to: ", books[bookIndex])
+    res.json(books[bookId])
+  }
+  else(res.status(404).send('Not found'));
+});
+
+//Delete book by ID
+app.delete('/books/:id', (req, res) => {
+  let bookId = req.params.id;
+  if(books[bookId]){
+    bookIndex = books.findIndex(obj => obj.id == bookId);
+    console.log("Book to be deleted: ", books[bookIndex]);
+    delete books[bookIndex];
+    console.log("Book deleted");
+    res.send("Book removed")
+  }
+  else(res.status(404).send('Not found'));
+})
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
