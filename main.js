6f21db62c6d70e5724610a63d59d8e8ab8435a2f
Ian de Heer <ian.deheer@gmail.com>
2024-07-10 19:48:15 -0700

    Configured authors to work with Postres DB

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ const app = express();
 const { Pool } = require('pg');
 const path = require('path'); // Importe o módulo 'path'
 
+const pool = new Pool({
+  user: 'admin',
+  password: 'admin123',
+  host: 'localhost',
+  port: 5432, // default Postgres port
+  database: 'bookstoreDb'
+});
+
 app.use(express.json()); // for parsing application/json
 
 const authors = [];
@@ -11,59 +19,83 @@ let authorCounter = 0;
 let bookCounter = 0;
 
 //Create author
-app.post('/authors', (req, res) => {
-  const newAuthor = {
-    "id": authorCounter,
-    "name": req.body.name,
-    "bio": req.body.bio
-  };
-  authors.push(newAuthor);
-  console.log(req.body);
-  authorCounter++;
-  res.send(req.body);
+app.post('/authors', async (req, res) => {
+  // below code works. just a different method of implementation
+  // const result = await pool.query(`INSERT INTO public.authors(name, bio) VALUES ('${req.body.name}', '${req.body.bio}')`);
+  const result = await pool.query('INSERT INTO public.authors(name, bio) VALUES ($1, $2)', [req.body.name, req.body.bio]);
+  
+  // if(result.rowCount){
+  //   res.send('Success');
+  // };
+  // Same as above
+  result.rowCount ? res.send('Success') : null;
+  
+  // const text = 'INSERT INTO users(name, email) VALUES($1, $2) RETURNING *'
+  // const values = ['brianc', 'brian.m.carlson@gmail.com']
+ 
+  // const res = await client.query(text, values) 
+  // const newAuthor = {
+  //   "name": req.body.name,
+  //   "bio": req.body.bio
+  // };
+  // authors.push(newAuthor);
+  // console.log(req.body);
+  // authorCounter++;
+  // res.send(req.body);
 });
 
 //Get all authors
-app.get('/authors', (req, res) => {
-  res.json(authors);
+app.get('/authors', async (req, res) => {
+  // res.json(authors);
+  const result = await pool.query('SELECT * FROM authors order by id');
+  if(result.rows.length == 0){
+    res.status(404).send('Not found');
+  }
+  else{
+    res.json(result.rows)
+  }
 });
 
 //Get author by ID
-app.get('/authors/:id', (req, res) => {
+app.get('/authors/:id', async (req, res) => {
   let authorId = req.params.id;
   console.log('get /authors/:id', authors[authorId]);
-  if(authors[authorId]){
-    res.json(authors[authorId]);
+  const result = await pool.query('SELECT * FROM authors WHERE id = $1', [authorId]);
+  // if(authors[authorId]){
+  //   res.json(authors[authorId]);
+  // }
+  // else(res.status(404).send('Not found'));
+  if(result.rows.length == 0){
+    res.status(404).send('Not found');
   }
-  else(res.status(404).send('Not found'));
+  else{
+    res.json(result.rows[0])
+  }
 });
 
 //Update author by ID
-app.put('/authors/:id', (req, res) => {
+app.put('/authors/:id', async (req, res) => {
   let authorId = req.params.id;
-  if(authors[authorId]){
-    authIndex = authors.findIndex(obj => obj.id == authorId);
-    console.log("Author updated from: ", authors[authIndex]);
-    authors[authIndex].name = req.body.name;
-    authors[authIndex].bio = req.body.bio;
-    console.log("to: ", authors[authIndex])
-    res.json(authors[authorId])
+  const result = await pool.query('UPDATE public.authors SET name=$1, bio=$2 WHERE id=$3;', [req.body.name, req.body.bio, authorId]);
+  if(result.rowCount == 1){
+    res.send('Success');
+  } 
+  else{
+    res.status(404).send('Not found');
   }
-  else(res.status(404).send('Not found'));
 });
 
 //Delete author by ID
-app.delete('/authors/:id', (req, res) => {
+app.delete('/authors/:id', async (req, res) => {
   let authorId = req.params.id;
-  if(authors[authorId]){
-    authIndex = authors.findIndex(obj => obj.id == authorId);
-    console.log("Author to be deleted: ", authors[authIndex]);
-    delete authors[authIndex];
-    console.log("Author deleted");
-    res.send("Author removed")
+  const result = await pool.query('DELETE FROM public.authors WHERE id=$1;', [authorId]);
+  if(result.rowCount == 1){
+    res.send('Success');
+  } 
+  else{
+    res.status(404).send('Not found');
   }
-  else(res.status(404).send('Not found'));
-})
+});
 
 //Create book
 app.post('/books', (req, res) => {
