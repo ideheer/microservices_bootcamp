2aa1b44ea10949d6f67d68d4f346ccf86314c05d
Ian de Heer <ian.deheer@gmail.com>
2024-07-10 23:13:01 -0700

    Tied all book operations/endpoints to pg db

--- main.js
+++ main.js
@@ -29,19 +29,6 @@ app.post('/authors', async (req, res) => {
   // };
   // Same as above
   result.rowCount ? res.send('Success') : null;
-  
-  // const text = 'INSERT INTO users(name, email) VALUES($1, $2) RETURNING *'
-  // const values = ['brianc', 'brian.m.carlson@gmail.com']
- 
-  // const res = await client.query(text, values) 
-  // const newAuthor = {
-  //   "name": req.body.name,
-  //   "bio": req.body.bio
-  // };
-  // authors.push(newAuthor);
-  // console.log(req.body);
-  // authorCounter++;
-  // res.send(req.body);
 });
 
 //Get all authors
@@ -61,10 +48,7 @@ app.get('/authors/:id', async (req, res) => {
   let authorId = req.params.id;
   console.log('get /authors/:id', authors[authorId]);
   const result = await pool.query('SELECT * FROM authors WHERE id = $1', [authorId]);
-  // if(authors[authorId]){
-  //   res.json(authors[authorId]);
-  // }
-  // else(res.status(404).send('Not found'));
+
   if(result.rows.length == 0){
     res.status(404).send('Not found');
   }
@@ -88,7 +72,7 @@ app.put('/authors/:id', async (req, res) => {
 //Delete author by ID
 app.delete('/authors/:id', async (req, res) => {
   let authorId = req.params.id;
-  const result = await pool.query('DELETE FROM public.authors WHERE id=$1;', [authorId]);
+  const result = await pool.query('DELETE FROM public.authors WHERE id=$1', [authorId]);
   if(result.rowCount == 1){
     res.send('Success');
   } 
@@ -98,64 +82,56 @@ app.delete('/authors/:id', async (req, res) => {
 });
 
 //Create book
-app.post('/books', (req, res) => {
-  const newBook = {
-    "id": bookCounter,
-    "title": req.body.title,
-	  "authorId": req.body.authorId,
-	  "publishedDate": req.body.publishedDate,
-	  "summary": req.body.summary
-  };
-  books.push(newBook);
-  console.log(req.body);
-  bookCounter++;
-  res.send(req.body);
+app.post('/books', async (req, res) => {
+  const result = await pool.query('INSERT INTO public.books(title, "publishedDate", "authorId", summary) VALUES ($1, $2, $3, $4)', [req.body.title, req.body.publishedDate, req.body.authorId, req.body.summary]);
+  result.rowCount ? res.send('Success') : null;
 });
 
 //Get all books
-app.get('/books', (req, res) => {
-  res.json(books);
+app.get('/books', async (req, res) => {
+  const result = await pool.query('SELECT * FROM books order by id');
+  if(result.rows.length == 0){
+    res.status(404).send('Not Found');
+  }
+  else{
+    res.json(result.rows)
+  }
 });
 
 //Get book by ID
-app.get('/books/:id', (req, res) => {
-  let bookId = req.params.id;
-  console.log('get /books/:id', books[bookId]);
-  if(books[bookId]){
-    res.json(books[bookId]);
+app.get('/books/:id', async (req, res) => {
+  const result = await pool.query('SELECT * FROM books WHERE id = $1', [req.params.id]);
+  if(result.rows.length == 0){
+    res.status(404).send('Not found');
   }
-  else(res.status(404).send('Not found'));
+  else{
+    res.json(result.rows[0])
+  }
 });
 
 //Update book by ID
-app.put('/books/:id', (req, res) => {
+app.put('/books/:id', async (req, res) => {
   let bookId = req.params.id;
-  if(books[bookId]){
-    bookIndex = books.findIndex(obj => obj.id == bookId);
-    console.log("Book updated from: ", books[bookIndex]);
-    books[bookIndex].title = req.body.title;
-    books[bookIndex].authorId = req.body.authorId;
-    books[bookIndex].publishedDate = req.body.publishedDate;
-    books[bookIndex].summary = req.body.summary;
-    console.log("to: ", books[bookIndex])
-    res.json(books[bookId])
+  const result = await pool.query('UPDATE public.books SET title=$1, "authorId"=$2, "publishedDate"=$3, summary=$4 WHERE id=$5;', [req.body.title, req.body.authorId, req.body.publishedDate, req.body.summary, bookId]);
+  if(result.rowCount == 1){
+    res.send('Success');
+  } 
+  else{
+    res.status(404).send('Not found');
   }
-  else(res.status(404).send('Not found'));
 });
 
 //Delete book by ID
-app.delete('/books/:id', (req, res) => {
+app.delete('/books/:id', async (req, res) => {
   let bookId = req.params.id;
-  if(books[bookId]){
-    bookIndex = books.findIndex(obj => obj.id == bookId);
-    console.log("Book to be deleted: ", books[bookIndex]);
-    delete books[bookIndex];
-    console.log("Book deleted");
-    res.send("Book removed")
+  const result = await pool.query('DELETE FROM public.books WHERE id=$1', [bookId]);
+  if(result.rowCount == 1){
+    res.send('Success');
   }
-  else(res.status(404).send('Not found'));
-})
-
+  else{
+    res.status(404).send('Not found');
+  }
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
