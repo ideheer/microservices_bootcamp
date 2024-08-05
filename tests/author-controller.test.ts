d5a9c314b54f97ae4905e11397b238741e9a2225
Ian de Heer <ian.deheer@gmail.com>
2024-08-05 13:08:59 -0700

    jest test

---
+++ tests/author-controller.test.ts
@@ -0,0 +1,48 @@
+import authorControllerFactory from '../src/controller/author';
+import pg from "pg";
+import Author from '../src/model/author';
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  user: 'admin',
+  password: 'admin123',
+  host: 'localhost',
+  port: 5432, // default Postgres port
+  database: 'bookstoreDb'
+});
+
+describe('Author Controller', () => {
+    it('should create an author successfully', async () => {
+        const resultingAuthor = {
+            name:'anyNameILike',
+            bio:'anyBioILike',
+            id:1898
+        };
+        const mockDb = {
+            query:function(queryCommand:String, values:Array<string>){
+                console.log(queryCommand, values)
+                return{
+                    rows:[resultingAuthor]
+                }
+            }
+        }
+        const mockReq = {
+            body: {
+                name:'anyNameILike',
+                bio:'anyBioILike'
+            }
+        }
+        let receivedData;
+        const mockRes = {
+            send:function(receivedAuthor:any){
+                console.log('Send function called', receivedAuthor);
+                receivedData = receivedAuthor;
+            }
+        }
+        const authorControllerObj = authorControllerFactory(mockDb as any);
+        await authorControllerObj.create(mockReq as any, mockRes as any)
+        expect(receivedData).toEqual(resultingAuthor)
+    });
+
+});
