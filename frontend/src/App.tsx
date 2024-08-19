509db88e987871cc6164bc44e2691f31e072ea33
James Winegar <jameswine@gmail.com>
2024-08-19 18:59:20 -0500

    update table

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,17 @@
-import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
-import './App.css'
+import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid";
+import "./App.css";
 
 const rows: GridRowsProp = [
-  { id: 1, col1: "Hello", col2: "World" },
-  { id: 2, col1: "DataGridPro", col2: "is Awesome" },
-  { id: 3, col1: "MUI", col2: "is Amazing" },
+  { id: 1, col1: "1", col2: "One" },
+  { id: 2, col1: "2", col2: "Two" },
+  { id: 3, col1: "3", col2: "Three" },
 ];
 
 const columns: GridColDef[] = [
-  { field: "col1", headerName: "Column 1", width: 150 },
-  { field: "col2", headerName: "Column 2", width: 150 },
+  { field: "col1", headerName: "Key", width: 200 },
+  { field: "col2", headerName: "Value", width: 200 },
 ];
+
 function App() {
   return (
     <div style={{ height: 300, width: "100%" }}>
@@ -19,4 +20,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
