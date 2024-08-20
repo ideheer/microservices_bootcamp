583b43fca03e74d67bfbec2622f152a2203f9875
James Winegar <jameswine@gmail.com>
2024-08-19 19:26:29 -0500

    get data from backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,49 @@
-import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const rows: GridRowsProp = [
-  { id: 1, col1: "1", col2: "One" },
-  { id: 2, col1: "2", col2: "Two" },
-  { id: 3, col1: "3", col2: "Three" },
-];
-
-const columns: GridColDef[] = [
-  { field: "col1", headerName: "Key", width: 200 },
-  { field: "col2", headerName: "Value", width: 200 },
-];
+interface AuthorData {
+  id: number;
+  name: string;
+  bio: number;
+}
 
 function App() {
+  const [data, setData] = useState<AuthorData[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/authors");
+        setData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 90 },
+    { field: "name", headerName: "Name", width: 150 },
+    { field: "bio", headerName: "Bio", width: 110 },
+  ];
+
   return (
-    <div style={{ height: 300, width: "100%" }}>
+    <div style={{ width: "100%" }}>
       <DataGrid
-        rows={rows}
+        rows={data}
         columns={columns}
         hideFooter={false}
         hideFooterPagination={false}
         hideFooterSelectedRowCount={false}
         paginationMode={"client"}
+        loading={loading}
       />
     </div>
   );
